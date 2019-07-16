84941f2d8f831a500aa0cd9117404ccd8a5f5b09
Kris Craig <KrisCraig@php.net>
2019-07-16 02:05:33 -0700

    Wireframe.

--- src/BirdieLib/BirdieLib.cs
+++ src/BirdieLib/BirdieLib.cs
@@ -4,6 +4,30 @@ namespace BirdieLib
 {
     public class BirdieLib
     {
+        public bool Active { get; private set; }
 
+        public BirdieLib()
+        {
+            Active = false;
+        }
+
+        public void Start()
+        {
+            Active = true;
+
+            // TODO - Launch ControlLoop thread.  --Kris
+        }
+
+        public void Stop()
+        {
+            Active = false;
+
+            // TODO - Stop thread.  --Kris
+        }
+
+        private void ControlLoop()
+        {
+            // TODO - Every hour, check followed Twitter accounts for new tweets and retweet.  --Kris
+        }
     }
 }
