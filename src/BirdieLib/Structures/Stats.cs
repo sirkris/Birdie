589e6e9bd7c1f65209a7dfbe8510db8017d07eb3
Kris Craig <KrisCraig@php.net>
2019-07-19 01:30:49 -0700

    Stats.

---
+++ src/BirdieLib/Structures/Stats.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace BirdieLib.Structures
+{
+    [Serializable]
+    public class Stats
+    {
+        [JsonProperty("retweets")]
+        public Dictionary<string, int> Retweets { get; set; }
+
+        [JsonProperty("firstRetweet")]
+        public DateTime? FirstRetweet { get; set; }
+
+        [JsonProperty("lastRetweet")]
+        public DateTime? LastRetweet { get; set; }
+
+        public Stats(Dictionary<string, int> retweets = null, DateTime? firstRetweet = null, DateTime? lastRetweet = null)
+        {
+            Retweets = retweets ?? new Dictionary<string, int>();
+            FirstRetweet = firstRetweet;
+            LastRetweet = lastRetweet;
+        }
+    }
+}
